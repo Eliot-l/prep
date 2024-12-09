fcf24962a589e2359513c1317f8ac552464fee83
eliot delmas <delmaseliot@gmail.com>
2024-12-09 15:11:19 +0100

    Model Wishlist

---
+++ db/migrate/20241209140449_create_wishlists.rb
@@ -0,0 +1,11 @@
+class CreateWishlists < ActiveRecord::Migration[7.1]
+  def change
+    create_table :wishlists do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.references :film, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
